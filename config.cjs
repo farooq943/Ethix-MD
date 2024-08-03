7772d9c8d214a77b530bd4288587ee11f115c2ed
farooq943 <162565404+farooq943@users.noreply.github.com>
2024-08-03 13:36:48 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "ETHIX-MD&7Ev5d6CS",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&CHxVSZhD",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
